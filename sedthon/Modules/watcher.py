4dc2c4d4ae6aa033d9434c36d0ca5e4f05b9be3d
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-08-21 03:46:35 +0300

    Update watcher.py

--- sedthon/Modules/watcher.py
+++ sedthon/Modules/watcher.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pytgcalls.types import AudioPiped, HighQualityAudio, Update
 
-from music-sed import BOT_ID, BOT_USERNAME, app, app2, Shahmdb, pytgcalls
+from music-sed import BOT_ID, BOT_USERNAME, app, app2, blald, pytgcalls
 from music-sed.Helpers import _clear_, buttons, gen_thumb
 
 welcome = 20
@@ -48,7 +48,7 @@ async def swr_handler(_, chat_id: int):
 async def on_stream_end(pytgcalls, update: Update):
     chat_id = update.chat_id
 
-    get = Shahmdb.get(chat_id)
+    get = blald.get(chat_id)
     if not get:
         try:
             await _clear_(chat_id)
