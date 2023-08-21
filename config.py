98df96d2a8c1bb1b6536df582e6ad2ea5f50599b
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-08-21 03:15:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://graph.org/file/d7f33299ed8c60ad82721.mp4")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/27f3254f92a6d4f2a9864.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Shahm_Sport")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ql8ql")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tipthon_help")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/E9N99")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6066647930").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1488114134").split()))
 
 
-FAILED = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+FAILED = "https://graph.org/file/a7a4ba8ac40b7f0bfb46f.jpg"
