6fa6ae67ed2093b7c11d07081dbf3155565334e1
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-08-21 03:50:50 +0300

    Update assistant.py

--- sedthon/Modules/assistant.py
+++ sedthon/Modules/assistant.py
@@ -2,7 +2,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ShahmMusic import ASS_MENTION, LOGGER, SUDOERS, app, app2
+from sedthon import ASS_MENTION, LOGGER, SUDOERS, app, app2
 
 
 @app.on_message(filters.command(["asspfp", "setpfp"]) | filters.command(["صوره","صورة"],prefixes= ["/", "!","","#"]) & SUDOERS)
