b7cea4a252e43b239d9e58afbb25ef5376f9085c
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-08-21 03:44:12 +0300

    Update clear.py

--- sedthon/Helpers/clear.py
+++ sedthon/Helpers/clear.py
@@ -1,11 +1,11 @@
 
-from ShahmMusic import Shahmdb
-from ShahmMusic.Helpers import remove_active_chat
+from music-sed import blald
+from music-sed.Helpers import remove_active_chat
 
 
 async def _clear_(chat_id):
     try:
-        Shahmdb[chat_id] = []
+        blald[chat_id] = []
         await remove_active_chat(chat_id)
     except:
         return
