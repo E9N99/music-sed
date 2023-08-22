3e4437752d63d69ec645f346252084add78cdb37
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-08-22 14:49:05 +0300

    Update watcher.py

--- Sedthon/Modules/watcher.py
+++ Sedthon/Modules/watcher.py
@@ -3,8 +3,8 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pytgcalls.types import AudioPiped, HighQualityAudio, Update
 
-from ShahmMusic import BOT_ID, BOT_USERNAME, app, app2, Shahmdb, pytgcalls
-from ShahmMusic.Helpers import _clear_, buttons, gen_thumb
+from Sedthon import BOT_ID, BOT_USERNAME, app, app2, Shahmdb, pytgcalls
+from Sedthon.Helpers import _clear_, buttons, gen_thumb
 
 welcome = 20
 close = 30
